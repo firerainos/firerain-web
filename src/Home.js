44bbe3ef8382fb1c2b9e68d16ce31d4d17e9cb73
xiayesuifeng <1377195627@qq.com>
2018-02-23 22:03:59 +0800

    remove .css

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
-import './Home.css'
 import Button from 'material-ui/Button'
 
 class Home extends Component {
+    classes = {
+        Home: {
+            background: '#3F51B5',
+            height: '800px'
+        }, logo: {
+            height: '250px',
+            width: '250px',
+            marginTop: '100px'
+        }, title: {
+            color: 'white'
+        }
+    };
+
     render() {
         return (
-            <div className="Home">
-                <img src={logo} alt="logo" className="logo"/>
-                <h1 className="title">FireRain OS</h1>
-                <Button variant="raised" color="secondary">立即下载</Button>
+            <div style={this.classes.Home}>
+                <img src={logo} alt="logo" style={this.classes.logo}/>
+                <h1 style={this.classes.title}>FireRain OS</h1>
+                <Button variant="raised" color="secondary" onClick={}>立即下载</Button>
             </div>
         )
     }
