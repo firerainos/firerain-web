5af4495df076ffd43b319b7eb7d22f610609319a
xiayesuifeng <1377195627@qq.com>
2018-02-25 21:41:13 +0800

    add deal with wiki tag

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Tabs, {Tab} from 'material-ui/Tabs';
 import Home from './Home'
 import Download from './Download'
 import SwipeableRoutes from "react-swipeable-routes";
-import { Route ,withRouter } from 'react-router-dom'
+import {Route, withRouter} from 'react-router-dom'
 import Package from "./Package";
 
 class App extends Component {
@@ -15,11 +15,15 @@ class App extends Component {
         value: 0,
     };
 
-    router = ['/', '/download','/package'];
+    router = ['/', '/download', '/package'];
 
     handleChange = (event, value) => {
-        this.setState({value});
-        this.props.history.push(this.router[value])
+        if (value === 3) {
+            window.location.href = 'https://wiki.firerain.xyz';
+        } else {
+            this.setState({value});
+            this.props.history.push(this.router[value])
+        }
     };
 
     componentWillReceiveProps(props) {
@@ -28,33 +32,33 @@ class App extends Component {
 
     render() {
         return (
-                <div className="App">
-                    <AppBar position="static">
-                        <Toolbar>
-                            <Typography variant="title" color="inherit">
-                                FireRain
-                            </Typography>
-                            <Tabs
-                                className="tags"
-                                value={this.state.value}
-                                onChange={this.handleChange}
-                                fullWidth
-                            >
-                                <Tab label="首页"/>
-                                <Tab label="下载"/>
-                                <Tab label="Packages"/>
-                                <Tab label="Wiki"/>
-                            </Tabs>
-                        </Toolbar>
+            <div className="App">
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="title" color="inherit">
+                            FireRain
+                        </Typography>
+                        <Tabs
+                            className="tags"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                            fullWidth
+                        >
+                            <Tab label="首页"/>
+                            <Tab label="下载"/>
+                            <Tab label="Packages"/>
+                            <Tab label="Wiki"/>
+                        </Tabs>
+                    </Toolbar>
 
-                    </AppBar>
-                    <SwipeableRoutes>
-                            <Route exact path="/" component={Home}/>
-                            <Route path="/download" component={Download}/>
-                            <Route path="/package" component={Package}/>
-                            {/*<Redirect to="/"/>*/}
-                    </SwipeableRoutes>
-                </div>
+                </AppBar>
+                <SwipeableRoutes>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/download" component={Download}/>
+                    <Route path="/package" component={Package}/>
+                    {/*<Redirect to="/"/>*/}
+                </SwipeableRoutes>
+            </div>
         );
     }
 }
