64289dead064d84ae70f32a837f7e9ce5045fa03
xiayesuifeng <1377195627@qq.com>
2018-02-25 18:50:00 +0800

    add package.js

---
+++ src/Package.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import image from './welcome_card.jpg'
+import TextField from 'material-ui/TextField';
+import Dialog, {
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from 'material-ui/Dialog';
+
+class Download extends Component {
+
+    render() {
+        return (
+            <div className="Package">
+
+            </div>
+        )
+    }
+}
+
+export default Download;
