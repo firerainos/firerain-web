25622ca9cf522dc2176cdf245dec574120da6e35
xiayesuifeng <1377195627@qq.com>
2018-02-28 17:08:52 +0800

    完成内侧资格审核基本功能

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -2,15 +2,7 @@ import React, {Component} from 'react';
 import Card, {CardActions, CardContent, CardMedia} from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
-import image from '../welcome_card.jpg'
-import TextField from 'material-ui/TextField';
 import axios from 'axios'
-import Dialog, {
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-} from 'material-ui/Dialog';
 
 class Download extends Component {
 
@@ -18,7 +10,7 @@ class Download extends Component {
         super(props, context);
         axios.get('/api/list/list')
             .then(r=>{
-                this.setState('lists',r.data)
+                this.setState({lists:r.data})
             })
     }
 
@@ -36,14 +28,54 @@ class Download extends Component {
         }
     };
 
+    handleDel(id) {
+        axios.delete('/api/list/delete?id='+id)
+            .then(r=>{
+                if(r.data.status==='success'){
+                    alert('删除成功')
+                }else{
+                    alert(r.data.error)
+                }
+            })
+            .catch(error=>{
+                alert('删除失败')
+            })
+    }
+
+    handlePass(id) {
+        axios.get('/api/list/pass?id='+id)
+            .then(r=>{
+                if(r.data.status==='success'){
+                    alert('邮件发送成功')
+                }else{
+                    alert(r.data.error)
+                }
+            })
+            .catch(error=>{
+                alert('邮件发送失败')
+            })
+    }
+
     render() {
         return (
             <div className="Admin">
-                <Card style={this.classes.card}>
-                    <CardContent>
-
-                    </CardContent>
-                </Card>
+                {this.state.lists.map((list) =>
+                    <Card style={this.classes.card} key={list.ID}>
+                        <CardContent>
+                            <p>{list.CreatedAt}</p>
+                            <p>{list.email}</p>
+                            <p>{list.qq}</p>
+                            <p>{list.region}</p>
+                            <p>{list.introduction}</p>
+                            <p>{list.suggest}</p>
+                            <p>{list.State}</p>
+                        </CardContent>
+                        <CardActions>
+                            <Button onClick={this.handleDel.bind(this,list.ID)}>删除</Button>
+                            <Button onClick={this.handlePass.bind(this,list.ID)}>PASS</Button>
+                        </CardActions>
+                    </Card>
+                )}
             </div>
         )
     }
