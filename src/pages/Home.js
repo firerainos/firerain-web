771e644ecec8a0422df4fb952d4d460ddc682ec0
xiayesuifeng <1377195627@qq.com>
2018-04-03 23:30:08 +0800

    use css in js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,35 +2,43 @@ import React, {Component} from 'react';
 import logo from '../logo.png';
 import Button from 'material-ui/Button'
 import {withRouter} from 'react-router-dom'
+import {withStyles} from "material-ui/styles/index";
+import PropTypes from "prop-types";
+
+const styles = theme => ({
+    header: {
+        background: theme.palette.primary.main,
+        height: '800px',
+        color: 'white'
+    },
+    logo: {
+        height: '250px',
+        width: '250px',
+        marginTop: '180px'
+    },
+    info: {
+        marginTop:'48px'
+    }
+});
 
 class Home extends Component {
-    classes = {
-        header: {
-            background: '#2196F3',
-            height: '800px',
-            color: 'white'
-        },
-        logo: {
-            height: '250px',
-            width: '250px',
-            marginTop: '180px'
-        },
-        info: {
-            marginTop:'48px'
-        }
-    };
+    constructor(props, context) {
+        super(props, context);
+    }
 
     render() {
+        const {classes} = this.props;
+
         return (
             <div>
-                <div style={this.classes.header}>
-                    <img src={logo} alt="logo" style={this.classes.logo}/>
+                <div className={classes.header}>
+                    <img src={logo} alt="logo" className={classes.logo}/>
                     <h1>FireRain OS</h1>
                     <p>FireRainOS是基于Arch Linux的用户友好的Linux发行版,提供图形操作,并保证Arch的原生性</p>
                     <Button variant="raised" color="secondary"
                             onClick={() => this.props.history.push('/download')}>立即下载</Button>
                 </div>
-                <div style={this.classes.info}>
+                <div className={classes.info}>
                     <h2>支持我们</h2>
                     <p>通过你的贡献、捐款或者赞助，FireRainOS 将获得繁荣发展。你的捐助直接用于支持我们付出工作、持续改进，最加重要的是有助于我们提供优秀的文档和资料！</p>
                 </div>
@@ -39,4 +47,8 @@ class Home extends Component {
     }
 }
 
-export default withRouter(Home);
+Home.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Home);
