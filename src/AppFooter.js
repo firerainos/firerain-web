80b0e5fde6f6b85d2bd04f6245668a1f6992678d
xiayesuifeng <1377195627@qq.com>
2018-04-03 23:59:40 +0800

    use css in js

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -2,55 +2,62 @@ import React, {Component} from 'react';
 import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid';
 import {Link} from "react-router-dom";
+import {withStyles} from "material-ui/styles/index";
+import PropTypes from "prop-types";
+
+const styles = theme => ({
+    root: {
+        overflow: 'auto',
+        textAlign:'left',
+        background:'#f7f7f7',
+        marginTop: '48px',
+    },
+    layout: {
+        padding: '48px',
+    },
+    list: {
+        margin: 0,
+        paddingLeft: 0,
+        listStyle: 'none',
+    },
+    listItem: {
+        paddingTop: '4px',
+        paddingBottom: '4px',
+    }
+});
 
 class AppFooter extends Component {
-
-    classes = {
-        root: {
-            overflow: 'auto',
-            textAlign:'left',
-            background:'#FAFAFA',
-            marginTop: '48px'
-        },
-        layout: {
-            padding: '48px',
-        },
-        list: {
-            margin: 0,
-            paddingLeft: 0,
-            listStyle: 'none',
-        },
-        listItem: {
-            paddingTop: '4px',
-            paddingBottom: '4px',
-        }
-    };
+    constructor(props, context) {
+        super(props, context);
+    }
 
     render() {
+        const {classes} = this.props;
+
         return (
-            <footer style={this.classes.root}>
-                <div style={this.classes.layout}>
+            <footer className={classes.root}>
+                <div className={classes.layout}>
                     <Typography variant="title" gutterBottom>
                         Quick Links
                     </Typography>
                     <Typography variant="subheading" component="div">
                         <Grid container spacing={0}>
                             <Grid item xs={12} sm={6}>
-                                <ul style={this.classes.list}>
-                                    <li style={this.classes.listItem}>
+                                <ul className={classes.list}>
+                                    <li className={classes.listItem}>
                                         <a href="https://github.com/firerainos">GitHub</a>
                                     </li>
-                                    <li style={this.classes.listItem}>
+                                    <li className={classes.listItem}>
                                         <a href="https://bbs.firerain.xyz">Community</a>
                                     </li>
                                 </ul>
                             </Grid>
                             <Grid item xs={12} sm={6}>
-                                <ul style={this.classes.list}>
-                                    <li style={this.classes.listItem}>
+                                <ul className={classes.list}>
+                                    <li className={classes.listItem}>
                                         <a href="https://www.archlinux.org">Arch Linux</a>
                                     </li>
-                                    <li style={this.classes.listItem}>
+                                    <li className={classes.listItem}>
                                         <a href="https://wiki.archlinux.org">Arch Wiki</a>
                                     </li>
                                 </ul>
@@ -63,4 +70,8 @@ class AppFooter extends Component {
     }
 }
 
-export default AppFooter
+AppFooter.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AppFooter);
