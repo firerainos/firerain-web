8da62de68eb610aee0fdde162fd424935d96e10a
xiayesuifeng <gdigrgg@gmail.com>
2018-07-05 00:48:24 +0800

    adapt to mobile devices

--- src/pages/Frame.js
+++ src/pages/Frame.js
@@ -1,20 +1,24 @@
-import React, {Component} from 'react';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Tabs, {Tab} from 'material-ui/Tabs';
+import React, { Component } from 'react'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import Tabs, { Tab } from 'material-ui/Tabs'
 import Home from './Home'
 import Download from './Download'
-import Profile from './profile';
-import Package from "./Package";
-import SwipeableRoutes from "react-swipeable-routes";
-import {Route} from 'react-router-dom'
-import AppFooter from "../AppFooter";
-import {withStyles} from "material-ui/styles/index";
-import PropTypes from "prop-types";
-import AccountFrame from "../widgets/AccountFrame";
-import Cookies from 'js-cookie';
+import Profile from './profile'
+import Package from './Package'
+import SwipeableRoutes from 'react-swipeable-routes'
+import { Link, Route } from 'react-router-dom'
+import AppFooter from '../AppFooter'
+import { withStyles } from 'material-ui/styles/index'
+import PropTypes from 'prop-types'
+import AccountFrame from '../widgets/AccountFrame'
+import Cookies from 'js-cookie'
 import axios from 'axios'
+import Hidden from 'material-ui/Hidden/Hidden'
+import { Divider, Drawer, IconButton, ListItemText, MenuItem, MenuList } from 'material-ui'
+import logo from '../logo.png'
+import MenuIcon from 'material-ui-icons/Menu'
 
 const styles = theme => ({
     Frame: {
@@ -30,67 +34,120 @@ const styles = theme => ({
     },
     tags: {
         marginRight: '30px'
+    },
+    logo: {
+        height: 200,
+        width: 200,
     }
-});
+})
 
 class Frame extends Component {
-    constructor(props, context) {
-        super(props, context);
+    constructor (props, context) {
+        super(props, context)
     }
 
     state = {
         value: 0,
-        user: Cookies.getJSON('user')
-    };
+        user: Cookies.getJSON('user'),
+        mobileOpen: false
+    }
 
-    router = ['/', '/download', '/package'];
+    router = ['/', '/download', '/package']
 
     handleChange = (event, value) => {
         if (value === 3) {
-            window.location.href = 'https://wiki.archlinux.org';
+            window.location.href = 'https://wiki.archlinux.org'
         } else {
-            this.setState({value});
+            this.setState({value})
             this.props.history.push(this.router[value])
         }
-    };
+    }
 
     handleLogup = () => {
         axios.post('/api/logout')
             .then(r => {
                 if (r.data.code === 0) {
-                    Cookies.remove("user");
+                    Cookies.remove('user')
                     this.setState({user: undefined})
                 } else {
-                    alert("退出失败")
+                    alert('退出失败')
                 }
-            });
-    };
+            })
+    }
 
-    componentWillReceiveProps(props) {
+    handleDrawerToggle = () => {
+        this.setState({mobileOpen: !this.state.mobileOpen})
+    }
+
+    componentWillReceiveProps (props) {
         this.state.value = this.router.indexOf(props.location.pathname)
     }
 
-    render() {
-        const {classes} = this.props;
+    render () {
+        const {classes} = this.props
 
         return (
             <div className={classes.Frame}>
+                <Hidden mdUp>
+                    <Drawer
+                        variant="temporary"
+                        open={this.state.mobileOpen}
+                        onClose={this.handleDrawerToggle}
+                        classes={{
+                            paper: classes.drawerPaper,
+                        }}
+                        ModalProps={{
+                            keepMounted: true,
+                        }}
+                    >
+                        <div className={classes.header}>
+                            <img src={logo} alt="logo" className={classes.logo}/>
+                            <Typography variant="title">{this.state.title}</Typography>
+                        </div>
+                        <Divider/>
+                        <MenuList>
+                            <MenuItem component={Link} to='/'>
+                                <ListItemText inset primary="首页"/>
+                            </MenuItem>
+                            <MenuItem component={Link} to='/download'>
+                                <ListItemText inset primary="下载"/>
+                            </MenuItem>
+                            <MenuItem component={Link} to='/package'>
+                                <ListItemText inset primary="Packages"/>
+                            </MenuItem>
+                            <MenuItem onClick={()=>{window.location.href = 'https://wiki.archlinux.org'}}>
+                                <ListItemText inset primary="Arch Wiki"/>
+                            </MenuItem>
+                        </MenuList>
+                    </Drawer>
+                </Hidden>
                 <AppBar position="static">
                     <Toolbar>
+                        <Hidden mdUp>
+                            <IconButton
+                                color="inherit"
+                                aria-label="open drawer"
+                                onClick={this.handleDrawerToggle}
+                            >
+                                <MenuIcon/>
+                            </IconButton>
+                        </Hidden>
                         <Typography variant="title" color="inherit" className={classes.flex}>
                             FireRain
                         </Typography>
-                        <Tabs
-                            className={classes.tags}
-                            value={this.state.value}
-                            onChange={this.handleChange}
-                            fullWidth
-                        >
-                            <Tab label="首页"/>
-                            <Tab label="下载"/>
-                            <Tab label="Packages"/>
-                            <Tab label="Arch Wiki"/>
-                        </Tabs>
+                        <Hidden smDown implementation="css">
+                            <Tabs
+                                className={classes.tags}
+                                value={this.state.value}
+                                onChange={this.handleChange}
+                                fullWidth
+                            >
+                                <Tab label="首页"/>
+                                <Tab label="下载"/>
+                                <Tab label="Packages"/>
+                                <Tab label="Arch Wiki"/>
+                            </Tabs>
+                        </Hidden>
                         {this.state.user !== undefined && <AccountFrame onLogup={this.handleLogup}/>}
                     </Toolbar>
 
@@ -103,12 +160,12 @@ class Frame extends Component {
                 </SwipeableRoutes>
                 <AppFooter/>
             </div>
-        );
+        )
     }
 }
 
 Frame.propTypes = {
     classes: PropTypes.object.isRequired,
-};
+}
 
-export default withStyles(styles)(Frame);
+export default withStyles(styles)(Frame)
